11e95aad3aa60893b03ab68180dd7776b9b42766
Christian <Christian@134.176.237.241>
2018-10-30 13:19:51 +0100

    Added result for Uebung 5.

--- src/uebung5/Fibonacci.java
+++ src/uebung5/Fibonacci.java
@@ -1,8 +1,20 @@
 package uebung5;
 
-import acm.program.Program;
+import acm.program.ConsoleProgram;
 
 @SuppressWarnings("serial")
-public class Fibonacci extends Program {
-
+public class Fibonacci extends ConsoleProgram {
+	public void run() {
+		int x = 0;
+		int y = 1;
+		println("F0 = " + x);
+		println("F1 = " + y);
+		
+		for (int i = 2; i < 20; i++) {
+			int z = x + y;
+			println("F" + i + " = " + z);
+			x = y;
+			y = z;
+		}
+	}
 }
